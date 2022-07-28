af7a355eb6a2024df991c912a3e284e1ce815b82
Narahari_snowflake <110186423+Narahari637095@users.noreply.github.com>
2022-07-28 22:15:16 +0530

    Update streamlit_app.py
    added emojis to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
 streamlit.title("my parents are preparing healthy dinner")
-
+  
 streamlit.header ("Break Fast Menu")
-streamlit.text(" Dosa with chutney")
-streamlit.text("Idly with vada sambar")
-streamlit.text("Puri with Kurma")
+streamlit.text(" Dosa with Chicken🐔")
+streamlit.text("Idly with vada sambar🥣 ")
+streamlit.text("Puri with Kurma🥗")
+streamlit.text("avacado with toast 🥑🍞")
