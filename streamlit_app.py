2ff65e3c4e0add7326b85aa8e01fba91911f1932
Narahari_snowflake <110186423+Narahari637095@users.noreply.github.com>
2022-07-28 22:11:02 +0530

    Update streamlit_app.py
    added header as breakfast
    added text as Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("my parents are preparing healthy dinner")
+
+streamlit.Header ("Break Fast Menu")
+streamlit.text(" Dosa with chutney")
+streamlit.text("Idly with vada sambar")
+streamlit.text*("Puri with Kurma")
