d5c9accf3205d64cba6191e677a0e3bd131d4e23
Narahari_snowflake <110186423+Narahari637095@users.noreply.github.com>
2022-07-28 22:12:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("my parents are preparing healthy dinner")
 
-streamlit.Header ("Break Fast Menu")
+streamlit.header ("Break Fast Menu")
 streamlit.text(" Dosa with chutney")
 streamlit.text("Idly with vada sambar")
-streamlit.text*("Puri with Kurma")
+streamlit.text("Puri with Kurma")
