4033d29f617c63041e13343604f9ebfd428fd39a
Narahari_snowflake <110186423+Narahari637095@users.noreply.github.com>
2022-08-04 22:03:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,26 +20,3 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #include display on badge
 streamlit.dataframe(fruits_to_show)
-
-streamlit.header('Fruityvice Fruit Advice!')
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
-streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output is the screen as table
-streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The Fruit Load list sontains")
-streamlit.text(my_data_rows)
