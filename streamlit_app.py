e9b3eab736f6f446328b59c35082d603061a50f0
Narahari_snowflake <110186423+Narahari637095@users.noreply.github.com>
2022-08-04 22:37:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output is the screen as table
+streamlit.dataframe(fruityvice_normalized)
