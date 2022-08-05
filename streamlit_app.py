54000434614ccfd9ebc776f89e3a5078e1a9257d
Narahari_snowflake <110186423+Narahari637095@users.noreply.github.com>
2022-08-05 14:54:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
 streamlit.title("my parents are preparing healthy dinner")
-  
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal🥣')
 streamlit.text('Kale, Spinach & Rocket Smoothie🥗')
@@ -10,7 +9,7 @@ streamlit.text("avacado with toast 🥑🍞")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
- import pandas
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
