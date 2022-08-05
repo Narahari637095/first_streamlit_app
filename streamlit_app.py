7fa9c7eb9c1a8e3f6be21905db440b1b8b571f5c
Narahari_snowflake <110186423+Narahari637095@users.noreply.github.com>
2022-08-05 14:51:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit
-import pandas
-import requests
-import snowflake.connector
 
 streamlit.title("my parents are preparing healthy dinner")
   
@@ -13,7 +10,7 @@ streamlit.text("avacado with toast 🥑🍞")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
- 
+ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -24,6 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #dispay badge
 streamlit.dataframe(fruits_to_show)
 
+import requests
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -36,4 +34,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output is the screen as table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
 
